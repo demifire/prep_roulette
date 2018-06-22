e869769ebaf235bf940d5cc0d7fae65982d813bb
Chaz Kondo <Chaz@2ppgn4x.local>
2018-06-21 21:22:38 -1000

    Finished

--- js/app.js
+++ js/app.js
@@ -14,3 +14,52 @@ A random number will be generated every 5 seconds.*/
 // 2) Create on and off buttons that will start and stop your application.
 
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+
+var button = document.createElement('div');
+button.id = 'button';
+button.innerHTML = 'Start da Roulette bah';
+button.addEventListener('click', start);
+document.body.appendChild(button);
+
+var button2 = document.createElement('div');
+button2.id = 'button2';
+button2.innerHTML = 'STOP IT';
+button2.addEventListener('click', stop);
+document.body.appendChild(button2);
+
+var timer;
+
+function start(){
+    console.log('started');
+    timer = setInterval(function(){
+        console.log('fired');
+        // var random = getRandomInt(0, 37);
+        var random = Math.floor(Math.random()*37)
+        console.log(random);
+        var div = document.createElement('div');
+        div.innerHTML = random;
+        div.className = 'num';
+        document.body.appendChild(div);
+        var daKine = document.getElementsByClassName('num');
+        for(var i=0;i<daKine.length;i++){
+          if(random%2!==0){
+            div.style.backgroundColor = 'red';
+            div.style.color = 'white';
+          }else if (random === 0){
+            div.style.backgroundColor = 'green';
+            div.style.color = 'white';
+          }else{
+            div.style.backgroundColor = 'black';
+            div.style.color = 'white';}
+          }
+        }, 200);
+};
+
+function stop(){
+    clearInterval(timer);
+}
